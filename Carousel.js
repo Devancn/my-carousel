de24c25fecd38207d9c2210a0de484f98d003725
dingxiaohuan <wydingxiaohuan@163.com>
2020-09-09 22:36:53 +0800

    feat: 轮播添加鼠标拖拽功能

--- Carousel.js
+++ Carousel.js
@@ -3,10 +3,12 @@ import { createElement, Text, Wrapper } from './createElement'
 import { Timeline, Animation } from './animation'
 import { ease } from './cubicBezier'
 
+
 export class Carousel {
     constructor(config) {
         this.children = [];
         this.attributes = new Map();
+        this.properties = new Map();
     }
 
     setAttribute(name, value) {
@@ -17,16 +19,51 @@ export class Carousel {
         this.children.push(child)
     }
     render() {
-        let children = this.data.map(url => {
-            let element = <img src={url} />;
+        let timeLine = new Timeline;
+        timeLine.start();
+
+        let position = 0;
+
+        let nextPicStopHandler = null
+
+        let children = this.data.map((url, currentPosition) => {
+            let lastPosition = (currentPosition - 1 + this.data.length) % this.data.length;
+            let nextPosition = (currentPosition + 1) % this.data.length;
+
+
+            let offset = 0;
+
+            let onStart = () => {
+                timeLine.pause()
+                clearTimeout(nextPicStopHandler)
+
+                let currentElement = children[currentPosition];
+                let currentTransformValue = Number(currentElement.style.transform.match(/^translateX\(([\s\S]+)px\)/)[1]);
+                offset = currentTransformValue + 500 * currentPosition;
+
+            }
+            let onPan = event => {
+                let lastElement = children[lastPosition];
+                let currentElement = children[currentPosition];
+                let nextElement = children[nextPosition];
+
+                let currentTransformValue = -500 * currentPosition + offset;
+                let lastTransformValue = - 500 - 500 * lastPosition + offset;
+                let nextTransformValue = 500 - 500 * nextPosition + offset;
+
+                let dx = event.clientX - event.startX;
+                console.log(`translateX(${lastTransformValue + dx})px`);
+                lastElement.style.transform = `translateX(${lastTransformValue + dx})px`;
+                currentElement.style.transform = `translateX(${currentTransformValue + dx})px`;
+                nextElement.style.transform = `translateX(${nextTransformValue + dx})px`;
+            }
+            let element = <img src={url} onStart={onStart} onPan={onPan} enableGesture={true} />;
+            element.style.transform = "translateX(0px)"
             element.addEventListener("dragstart", event => event.preventDefault())
             return element
         })
 
-        let position = 0;
 
-        let timeLine = new Timeline;
-        timeLine.start();
 
         let nextPic = () => {
             let nextPosition = (position + 1) % this.data.length;
@@ -37,21 +74,21 @@ export class Carousel {
             let currentAnimation = new Animation(current.style, "transform",
                 -100 * position,
                 -100 - 100 * position,
-                500, 0, ease, v => `translateX(${v}%)`
+                500, 0, ease, v => `translateX(${5 * v}px)`
             );
             let nextAnimation = new Animation(next.style, "transform",
                 100 - 100 * nextPosition,
                 -100 * nextPosition,
-                500, 0, ease, v => `translateX(${v}%)`
+                500, 0, ease, v => `translateX(${5 * v}px)`
             );
 
             timeLine.add(currentAnimation);
             timeLine.add(nextAnimation);
 
             position = nextPosition;
-            setTimeout(nextPic, 3000)
+            nextPicStopHandler = setTimeout(nextPic, 3000)
         }
-        setTimeout(nextPic, 3000)
+        nextPicStopHandler = setTimeout(nextPic, 3000)
 
         return <div class="carousel">
             {children}
--- createElement.js
+++ createElement.js
@@ -1,43 +1,5 @@
-export class Text {
-    constructor(text) {
-        this.children = [];
-        this.root = document.createTextNode(text);
-    }
-    mountTo(parent) {
-        parent.appendChild(this.root);
-    }
-}
+import { enableGesture } from './gesture'
 
-export class Wrapper {
-    constructor(type) {
-        this.children = [];
-        this.root = document.createElement(type);
-    }
-
-    setAttribute(name, value) {
-        this.root.setAttribute(name, value)
-    }
-
-    appendChild(child) {
-        this.children.push(child)
-    }
-
-    addEventListener() {
-        this.root.addEventListener(...arguments)
-    }
-
-    get style() {
-        return this.root.style;
-    }
-
-    mountTo(parent) {
-        parent.appendChild(this.root)
-
-        for (let child of this.children) {
-            child.mountTo(this.root)
-        }
-    }
-}
 export function createElement(Cls, attributes, ...children) {
     let o;
     if (typeof Cls === 'string') {
@@ -65,4 +27,55 @@ export function createElement(Cls, attributes, ...children) {
     }
     visit(children)
     return o;
+}
+
+
+export class Text {
+    constructor(text) {
+        this.children = [];
+        this.root = document.createTextNode(text);
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+
+export class Wrapper {
+    constructor(type) {
+        this.children = [];
+        this.root = document.createElement(type);
+    }
+
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value)
+
+        if (name.match(/^on([\s\S]+)$/)) {
+            let eventName = RegExp.$1.replace(/^[\s\S]/, c => c.toLocaleLowerCase());
+            this.addEventListener(eventName, value)
+        }
+
+        if (name === "enableGesture") {
+            enableGesture(this.root)
+        }
+    }
+
+    appendChild(child) {
+        this.children.push(child)
+    }
+
+    addEventListener() {
+        this.root.addEventListener(...arguments)
+    }
+
+    get style() {
+        return this.root.style;
+    }
+
+    mountTo(parent) {
+        parent.appendChild(this.root)
+
+        for (let child of this.children) {
+            child.mountTo(this.root)
+        }
+    }
 }
